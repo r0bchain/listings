2ff6f5298f4908e06b783be16cb4d4e49a87488c
Rob <robbyschuh@gmail.com>
2024-08-02 12:37:15 +0700

    Filter form Markup && Styles

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,10 +1,10 @@
 <template>
-    
+    <Filters />
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />
 
     </div>
-    <div v-if="listings.data.length" class="w-full justify-center mt-8 mb-8">
+    <div v-if="listings.data.length" class="w-full justify-center mt-4 mb-4">
         <Pagination :links="listings.links" />
     </div>
 </template>
@@ -14,6 +14,7 @@
 import Listing from '@/Pages/Listing/Index/Components/Listing.vue'
 import ErrorMessage from '@/Components/Messages/ErrorMessage.vue'
 import Pagination from '@/Components/UI/Pagination.vue';
+import Filters from '@/Pages/Listing/Index/Components/Filters.vue';
 
 defineProps( {
     listings: Object,
---
+++ resources/js/Pages/Listing/Index/Components/Filters.vue
@@ -0,0 +1,38 @@
+<template>
+    <form>
+        <div class="mb-8 mt-4 flex flex-wrap gap-2">
+            <div class="flex flex-nowrap items-center">
+                <input type="text" class="input-filter-l w-28"  placeholder="Price from" />
+                <input type="text" class="input-filter-r w-28"  placeholder="Price to"/>
+            </div>
+            <div class="flex flex-nowrap items-center">
+               <select class="input-filter-l w-22">
+                 <option :value="null">Beds</option>
+                 <option v-for="n in 5" :key="n" :value="n" v-text="n" />
+                 <option>6+</option>
+
+               </select>
+          
+               <select class="input-filter-r w-22">
+                 <option :value="null">Baths</option>
+                 <option v-for="n in 5" :key="n" :value="n" v-text="n" />
+                 <option>6+</option>
+
+               </select>
+            </div>
+
+            <div class="flex flex-nowrap items-center">
+                <input type="text" class="input-filter-l w-28" placeholder="Area from" />
+                <input type="text" class="input-filter-r w-28" placeholder="Area to"/>
+            </div>
+
+            <button type="submit" class="btn-normal">Filters</button>
+            <button type="reset">Clear</button>
+
+        </div>
+        
+    </form>
+</template>
+
+<script setup> 
+</script>
