60dbf6e8729b0343623344b03ef25447080dfe78
Rob <robbyschuh@gmail.com>
2024-08-02 17:57:32 +0700

    refactor

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -24,40 +24,41 @@ class ListingController extends Controller
     public function index(Request $request)
     {
         $filters = $request->only(['priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo']);
-        $query = Listing::orderByDesc('created_at');
-
-       
-        if ($filters['priceFrom'] ?? false) {
-            $query->where('price', '>=', $filters['priceFrom']);
-        }
-
-        if ($filters['priceTo'] ?? false) {
-            $query->where('price', '<=', $filters['priceTo']);
-        }
-
-        if ($filters['beds'] ?? false) {
-            $query->where('beds', $filters['beds']);
-        }
-
-        if ($filters['baths'] ?? false) {
-            $query->where('baths', $filters['baths']);
-        }
-
-        if ($filters['areaFrom'] ?? false) {
-            $query->where('area', '>=', $filters['areaFrom']);
-        }
-
-        if ($filters['areaTo'] ?? false) {
-            $query->where('area', '<=', $filters['areaTo']);
-        }
-
+        //$query = Listing::orderByDesc('created_at');
 
         return  inertia('Listing/Index',
         [
             // Returns an array with the listings
             // 'listings' => Listing::all()
             // Returns an object with the listings and the pagination links
-            'listings' => $query->paginate(10)->withQueryString(),
+            'listings' => 
+            Listing::orderByDesc('created_at')
+            ->when(
+                $filters['priceFrom'] ?? false, 
+                // Only executed in the first expression is TRUE.
+                fn ($query, $value) => $query->where('price', '>=', $value)
+            )->when(
+                $filters['priceTo'] ?? false, 
+                // Only executed in the first expression is TRUE.
+                fn ($query, $value) => $query->where('price', '<=', $value)
+            )->when(
+                $filters['beds'] ?? false, 
+                // Only executed in the first expression is TRUE.
+                fn ($query, $value) => $query->where('beds', (int)$value < 6 ? '=' : '>=', $value)
+            )->when(
+                $filters['baths'] ?? false, 
+                // Only executed in the first expression is TRUE.
+                fn ($query, $value) => $query->where('baths', (int)$value < 6 ? '=' : '>=', $value)
+            )->when(
+                $filters['areaFrom'] ?? false, 
+                // Only executed in the first expression is TRUE.
+                fn ($query, $value) => $query->where('area', '>=', $value)
+            )->when(
+                $filters['areaTo'] ?? false, 
+                // Only executed in the first expression is TRUE.
+                fn ($query, $value) => $query->where('area', '<=', $value)
+            )->paginate(10)->withQueryString(),
+
             'filters' => $request->only(['priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo']),
 
         ]
