690ead77cab50f26f17acc1f24f56be570ba9fd6
Rob <robbyschuh@gmail.com>
2024-10-07 13:22:46 +0700

    updated Register form

--- resources/js/Pages/UserAccount/Components/RegisterBox.vue
+++ resources/js/Pages/UserAccount/Components/RegisterBox.vue
@@ -1,10 +1,10 @@
 <!-- component -->
  <template>
     <form @submit.prevent="register">
-        <div class="topic-title "> {{  imageUpdated.query  }}</div>
+        <div class="topic-title "> {{  imageUpdated  }}</div>
         <div
         
-        :style="{ backgroundImage: `url(${imageUpdated.imageUrl})` }"
+        :style="{ backgroundImage: `url(${imageUpdated})` }"
         class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
             <div class="relative py-3 sm:max-w-xl sm:mx-auto">
                 <div class="absolute inset-0 bg-gradient-to-r from-blue-300 to-red-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
@@ -70,9 +70,15 @@ import { ref} from 'vue'
 const page = usePage()
 const props = defineProps({
     randomImage: {
-        type: Object,
+        type: String,
         Required: true
     },
+
+    topic: {
+        type: String,
+        Required: false,
+        default: 'Thailand'
+    }
     
 })
 
--- resources/js/Pages/UserAccount/Create.vue
+++ resources/js/Pages/UserAccount/Create.vue
@@ -1,36 +1,34 @@
 <template>
-    <RegisterBox :randomImage="randomImage" :location="randomImage" />
+    <RegisterBox :randomImage="topicImage" :topic="topic" />
   
 </template>
 
 <script setup>
-import { useForm, Link, usePage  } from '@inertiajs/vue3'
-import { ref} from 'vue'
+import { usePage  } from '@inertiajs/vue3'
+import {ref, onMounted} from 'vue'
 import RegisterBox from '@/Pages/UserAccount/Components/RegisterBox.vue'
-import { randomImages } from '@/Composables/randomImages';
 import { searchIPFSImage } from '@/Composables/searchIPFSImage';
 
 const page = usePage()
 
 const props = defineProps({ listing: Object })
+const topicImage = ref(null)
 
-const imageUrl = ref('https://images.pexels.com/photos/2956618/pexels-photo-2956618.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800')
+// Get a random topic
+const topic = ref(page.props.site.TOPICS_IMAGE[Math.floor(Math.random() * page.props.site.TOPICS_IMAGE.length)])
 
-const topics = page.props.site.TOPICS_IMAGE
 
 // Fetch the image URL on component mount
 
-const { ipfsTopicImage } = searchIPFSImage(
-    topics[0],
-    page.props.config.PINATA_SECRET_JWT,
-    page.props.config.PINATA_GATEWAY
-)
+onMounted(async () => {
+    try {
+        const result = await searchIPFSImage(topic.value.trim(), page.props.config.PINATA_SECRET_JWT, page.props.config.PINATA_GATEWAY);
+        console.log('result', result);
+        topicImage.value = result;
+    } catch (error) {
+        console.log('error', error);
+    }
+});
 
-const { randomImage, location } = randomImages(
-    imageUrl,
-    page.props.site.RANDOM_IMAGE_KEY, 
-    topics,
-    {per_page: 1, size: 'small', locale: 'en-US', orientation: 'landscape' }
-)
 
 </script>
